626bd9a94f7188020b99ea1aff42af52f378f0ec
JosueJMartinez <jmarti61@gmail.com>
2020-09-26 00:09:07 -0500

    updated added copy functionality

--- src/js/components/ColorBox.jsx
+++ src/js/components/ColorBox.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import '../../css/ColorBox.css';
 
 export default class ColorBox extends Component {
 	render() {
 		const { color, name } = this.props;
 		return (
-			<div
-				className="ColorBox"
-				style={{ backgroundColor: color }}
-				id={color}
-			>
-				<div className="copy-container">
-					<div className="content">
-						<div className="color-name">{name}</div>{' '}
-						<button className="copy-button">Copy</button>
-						<div className="more-link">More</div>
+			<CopyToClipboard text={color}>
+				<div
+					className="ColorBox"
+					style={{ backgroundColor: color }}
+					id={color}
+				>
+					<div className="copy-container">
+						<div className="content">
+							<div className="color-name">{name}</div>{' '}
+							<button className="copy-button">Copy</button>
+							<div className="more-link">More</div>
+						</div>
 					</div>
 				</div>
-			</div>
+			</CopyToClipboard>
 		);
 	}
 }

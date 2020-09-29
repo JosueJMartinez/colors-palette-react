e9f5122a77a516b2b9e2d3e0d447d89eee1a4682
JosueJMartinez <jmarti61@gmail.com>
2020-09-29 13:08:58 -0500

    updated paletteList still working on it

--- src/js/components/MiniPalette.jsx
+++ src/js/components/MiniPalette.jsx
@@ -1,16 +1,61 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import { Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
 
 const styles = {
-	root: {
-		backgroundColor: 'purple',
-		border: '3px solid teal'
+	root: {},
+	card: {
+		backgroundColor: 'white',
+		borderRadius: '5px',
+		padding: '0.5rem'
+		// height: '150px',
+	},
+	colors: {
+		height: '200px'
+	},
+	color: {
+		height: '40px'
+	},
+	title: {
+		display: 'flex',
+		justifyContent: 'space-between'
+		// color: '#2c1515'
+	},
+	link: {
+		textDecoration: 'none',
+		fontSize: '1.5rem'
 	}
 };
 
 function MiniPalette(props) {
-	const { classes } = props;
-	return <div className={classes.root}>MiniPalette</div>;
+	const { classes, id, paletteName, emoji, colors } = props;
+	return (
+		<Grid className={classes.root} id={id} item xs={12} sm={6} md={4}>
+			<Link className={classes.link} to={`/palette/${id}`}>
+				<Card className={classes.card}>
+					<div className={classes.colors}>
+						<Grid container>
+							{colors.map((c, idx) => (
+								<Grid
+									className={classes.color}
+									item
+									xs={3}
+									style={{ backgroundColor: c.color }}
+									key={idx}
+								/>
+							))}
+						</Grid>
+					</div>
+					<h5 className={classes.title}>
+						<span>{paletteName}</span>{' '}
+						<span className={classes.emoji}>{emoji}</span>
+					</h5>
+				</Card>
+			</Link>
+		</Grid>
+	);
 }
 
 export default withStyles(styles)(MiniPalette);
--- src/js/components/PaletteList.jsx
+++ src/js/components/PaletteList.jsx
@@ -1,38 +1,34 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 import MiniPalette from './MiniPalette';
 
 export default class PaletteList extends Component {
 	render() {
 		const { palettes } = this.props;
-		const useStyles = makeStyles((theme) => ({
-		  root: {
-			flexGrow: 1,
-		  },
-		  paper: {
-			padding: theme.spacing(2),
-			textAlign: 'center',
-			color: theme.palette.text.secondary,
-		  },
+		const useStyles = makeStyles(theme => ({
+			root: {
+				flexGrow: 1
+			},
+			paper: {
+				padding: theme.spacing(2),
+				textAlign: 'center',
+				color: theme.palette.text.secondary
+			}
 		}));
 		return (
-			
-			
-			<Grid container spacing={1}>
-				<Grid item xs={12}><h1>List of Palette Colors</h1></Grid>
-				<Grid item xs={12}><MiniPalette /></Grid>	
-				
-				{palettes.map(p => (
-				<Grid key={p.id} id={p.id} item xs={12} sm={6} md={4} lg={3}>
-					<Link to={`/palette/${p.id}`}>{p.paletteName}</Link>
+			<Container fixed>
+				<Grid container spacing={2}>
+					<Grid item xs={12}>
+						<h1>List of Palette Colors</h1>
+					</Grid>
+					{palettes.map(p => <MiniPalette key={p.id} {...p} />)}
 				</Grid>
-				))}
-				
-			</Grid>
+			</Container>
 		);
 	}
 }

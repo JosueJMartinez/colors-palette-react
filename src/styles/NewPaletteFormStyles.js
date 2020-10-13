e8b455450901cd1122030771c0546c9aea94a878
JosueJMartinez <jmarti61@gmail.com>
2020-10-13 05:24:02 +0000

    updated palette form

--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -36,8 +36,12 @@ export default function styles(theme, drawerWidth) {
       padding: theme.spacing(0, 1),
       // necessary for content to be below app bar
       ...theme.mixins.toolbar,
-      justifyContent: "flex-end",
+      justifyContent: "space-between",
     },
+	  drawerHeaderTitle:{
+		marginLeft:'auto',
+		marginRight:'auto'
+	  },
     content: {
       flexGrow: 1,
       padding: theme.spacing(3),

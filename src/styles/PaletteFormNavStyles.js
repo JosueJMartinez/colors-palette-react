5329bad527b38546ad7f2e99c5411d2de33f5cdd
JosueJMartinez <jmarti61@gmail.com>
2020-10-18 14:18:43 -0500

    added missing file from last commit

---
+++ src/styles/PaletteFormNavStyles.js
@@ -0,0 +1,35 @@
+export default function styles(theme, drawerWidth) {
+  return {
+    root: { display: "flex" },
+    appBar: {
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      height: "64px",
+    },
+    toolBar: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: "none",
+    },
+    paletteNameForm: {
+      display: "flex",
+      flexDirection: "row",
+    },
+    appBarBtns: {},
+  };
+}

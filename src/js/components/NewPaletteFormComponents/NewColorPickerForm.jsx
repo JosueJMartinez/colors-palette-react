71a1cef05449cf152e50aa3bd878a825056a3908
JosueJMartinez <jmarti61@gmail.com>
2020-10-18 00:29:34 -0500

    removed bug color clears when chose a new color

--- src/js/components/NewPaletteFormComponents/NewColorPickerForm.jsx
+++ src/js/components/NewPaletteFormComponents/NewColorPickerForm.jsx
@@ -34,7 +34,6 @@ export default function NewColorPickerForm(props) {
     setState(prevState => ({
       ...prevState,
       currentColor: color.hex,
-      newColorName: "",
     }));
   };
   const handleNameChange = e => {

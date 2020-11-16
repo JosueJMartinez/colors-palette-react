9828ba2da3e7196a5c13d0be5f0b5f6ee1e66402
JosueJMartinez <jmarti61@gmail.com>
2020-11-16 02:13:30 -0600

    Updates

---
+++ precache-manifest.c616589eef949fefc75943061a7364b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355e4e885b568ff42c46eb3a734dae7e",
+    "url": "/react-yahtzee/index.html"
+  },
+  {
+    "revision": "6e6dbac3132fadba5b7b",
+    "url": "/react-yahtzee/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "df6211fec192364a8f51",
+    "url": "/react-yahtzee/static/css/main.3a33e34e.chunk.css"
+  },
+  {
+    "revision": "6e6dbac3132fadba5b7b",
+    "url": "/react-yahtzee/static/js/2.8b9b98f7.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-yahtzee/static/js/2.8b9b98f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6211fec192364a8f51",
+    "url": "/react-yahtzee/static/js/main.f17bcea8.chunk.js"
+  },
+  {
+    "revision": "18859726884f7a7a0d2d",
+    "url": "/react-yahtzee/static/js/runtime-main.a8e2b2bd.js"
+  },
+  {
+    "revision": "850989fb55036f1565fff4e893e1eff3",
+    "url": "/react-yahtzee/static/media/bg-tile.850989fb.png"
+  }
+]);
